e57faa0df5579bca21db54ae2770d0305c014847
iskander.sierra@gmail.com <iskander.sierra@gmail.com>
2016-07-22 23:19:40 +0200

    Created nuget package

--- FsJson/AssemblyInfo.fs
+++ FsJson/AssemblyInfo.fs
@@ -8,9 +8,9 @@ open System.Runtime.InteropServices
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [<assembly: AssemblyTitle("FsJson")>]
-[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyDescription("Converters for basic F# types using Newtonsoft.Json")>]
 [<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyCompany("iskandersierra")>]
 [<assembly: AssemblyProduct("FsJson")>]
 [<assembly: AssemblyCopyright("Copyright ©  2016")>]
 [<assembly: AssemblyTrademark("")>]
@@ -34,8 +34,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("0.1.0-alpha.0")>]
+[<assembly: AssemblyFileVersion("0.1.0-alpha.0")>]
 
 do
     ()
